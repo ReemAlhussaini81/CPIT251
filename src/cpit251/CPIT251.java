fe46ba3ece2a2c290cea83f628ee3f163d69c425
Reem_Alhussaini <Reemalhussaini81@outlook.com>
2023-03-18 02:20:18 +0300

    first commit

---
+++ src/cpit251/CPIT251.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cpit251;
+
+/**
+ *
+ * @author ryo0m
+ */
+public class CPIT251 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
